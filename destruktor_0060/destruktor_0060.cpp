9c4ab706c05c99eda793a34f0b268894a2520b3d
maskikuy <kproject305@gmail.com>
2024-05-13 21:09:10 +0700

    menuliskan header chainfunction

--- destruktor_0060/destruktor_0060.cpp
+++ destruktor_0060/destruktor_0060.cpp
@@ -39,8 +39,9 @@ void angka::isiData() {
 }
 
 int main() {
-    angka belajarcpp(3); 
+    angka belajarcpp(3);
     angka* ptrBelajarcpp = new angka(5);
-    delete ptrBelajarcpp; 
+    delete ptrBelajarcpp;
 
     return 0;
+}
