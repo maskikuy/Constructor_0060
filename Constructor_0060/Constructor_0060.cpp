17c88a44352d89dfdc1625d7cbf9b33511ac530a
maskikuy <kproject305@gmail.com>
2024-05-13 14:49:56 +0700

    melengkapi int main dengan return 0

--- Constructor_0060/Constructor_0060.cpp
+++ Constructor_0060/Constructor_0060.cpp
@@ -37,5 +37,14 @@ int main()
 	mahasiswa mhs1("Lia Kurnia");
 	mahasiswa mhs2("Asroni");
 	mahasiswa mhs3("Andi Kurniawan");
-	mahasiswa mhs3("Joko Purba");
+	mahasiswa mhs4("Joko Purba");
+
+
+	mhs1.printAll();
+
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+	return 0;
+
 }
