36d0d3e30a2b43909aeac28f94813887571bf838
maskikuy <kproject305@gmail.com>
2024-05-13 21:08:06 +0700

    memangil destruktor

--- destruktor_0060/destruktor_0060.cpp
+++ destruktor_0060/destruktor_0060.cpp
@@ -37,3 +37,10 @@ void angka::isiData() {
     }
     cout << endl;
 }
+
+int main() {
+    angka belajarcpp(3); 
+    angka* ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp; 
+
+    return 0;
