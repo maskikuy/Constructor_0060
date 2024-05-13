05952358e46f4cc0d230b884e6ff27ca47938c26
maskikuy <kproject305@gmail.com>
2024-05-13 21:06:25 +0700

    memasukan class angka

--- destruktor_0060/destruktor_0060.cpp
+++ destruktor_0060/destruktor_0060.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class angka {
+private:
+    int* arr;
+    int panjang;
+public:
+    angka(int); //Constuktor
+    ~angka(); //Destruktor
+
+    void cetakData();
+    void isiData();
+};
